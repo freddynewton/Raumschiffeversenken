d4f6c27fdca6a3ad4a7ab54781147b1f9a00b490
fa019 <fa019@hdm-stuttgart.de>
2018-03-30 12:24:52 +0200

    Raumschiff Klasse ist fertig Etwas am Spieler und der Zelle versucht
    // Docu

--- src/main/java/RaumSchiffeVersenken/RaumSchiff.java
+++ src/main/java/RaumSchiffeVersenken/RaumSchiff.java
@@ -1,4 +1,32 @@
 package RaumSchiffeVersenken;
 
+
 public class RaumSchiff {
+    public int laenge;
+    public int Model;
+    public int xKoordinaten;
+    public int yKoordinaten;
+
+    /**
+     * @param laenge       Das beschreibt mit dem Model die Größe und dementsprechend dann
+     *                     auch das Model.
+     * @param model        1 = Jäger
+     *                     2 = Frigatte
+     *                     4 = Zerstörer
+     *                     5 = Kreuzer
+     * @param xKoordinaten
+     * @param yKoordinaten
+     */
+    public RaumSchiff(int laenge, int model, int xKoordinaten, int yKoordinaten) {
+        this.laenge = laenge;
+        Model = model;
+        this.xKoordinaten = xKoordinaten;
+        this.yKoordinaten = yKoordinaten;
+    }
+
+   /*
+   Beispiel wie man ein Schiff erstellt mit dem Constructor
+
+    RaumSchiff zerstörer = new RaumSchiff(4,4, 3, 10);
+     */
 }
--- src/main/java/RaumSchiffeVersenken/Zelle.java
+++ src/main/java/RaumSchiffeVersenken/Zelle.java
@@ -1,4 +1,17 @@
 package RaumSchiffeVersenken;
 
 public class Zelle {
+    private boolean raumschiff = false;
+    private int Status = 0;
+
+
+    /**
+     * @return Den Status
+     */
+    public int getStatus() {
+        return Status;
+    }
+
+
+
 }
