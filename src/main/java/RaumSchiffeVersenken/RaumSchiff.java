b6a8b436b1d3901899017a3d65fab93c8e403c0d
fa019 <fa019@hdm-stuttgart.de>
2018-04-04 09:18:38 +0200

    Arbeiten an der Klassenübergreifenden OOP // Docu

--- src/main/java/RaumSchiffeVersenken/RaumSchiff.java
+++ src/main/java/RaumSchiffeVersenken/RaumSchiff.java
@@ -19,14 +19,46 @@ public class RaumSchiff {
      */
     public RaumSchiff(int laenge, int model, int xKoordinaten, int yKoordinaten) {
         this.laenge = laenge;
-        Model = model;
+        this.Model = model;
         this.xKoordinaten = xKoordinaten;
         this.yKoordinaten = yKoordinaten;
     }
-
-   /*
+/*
    Beispiel wie man ein Schiff erstellt mit dem Constructor
 
     RaumSchiff zerstörer = new RaumSchiff(4,4, 3, 10);
      */
+
+
+    public int getLaenge() {
+        return laenge;
+    }
+
+    public void setLaenge(int laenge) {
+        this.laenge = laenge;
+    }
+
+    public int getModel() {
+        return Model;
+    }
+
+    public void setModel(int model) {
+        Model = model;
+    }
+
+    public int getxKoordinaten() {
+        return xKoordinaten;
+    }
+
+    public void setxKoordinaten(int xKoordinaten) {
+        this.xKoordinaten = xKoordinaten;
+    }
+
+    public int getyKoordinaten() {
+        return yKoordinaten;
+    }
+
+    public void setyKoordinaten(int yKoordinaten) {
+        this.yKoordinaten = yKoordinaten;
+    }
 }
