558b5382c6961d7dc75023a7b4f27f4305d24e41
fa019 <fa019@hdm-stuttgart.de>
2018-04-09 15:12:54 +0200

    Objekte erstellt und toString() verschönert mit " "; // Docu

--- src/main/java/RaumSchiffeVersenken/Core/SpielSystemMain.java
+++ src/main/java/RaumSchiffeVersenken/Core/SpielSystemMain.java
@@ -5,12 +5,21 @@ public class SpielSystemMain {
         Feld Feld1 = new Feld(10, 10);
         System.out.println(Feld1.toString());
 
+        int SchiffAuswahl = 0;
+
+        switch (SchiffAuswahl) {
+            case 0:
+        }
+
+        Feld1.zielenZumSchiffeSetzen();
+
+        System.out.println("\n");
+        System.out.println(Feld1.toString());
 
         for (int i = 0; i < 99; i++) {
             Feld1.zielenZumSchiessen();
             System.out.println("\n");
             System.out.println(Feld1.toString());
-
         }
 
 
