9acf180769ba8bdfec4868636b9b9eaec8a9deaa
Serafi Rajan <rs113@hdm-stuttgart.de>
2018-05-03 17:12:18 +0200

    Erste Test Vorschläge.

--- src/test/java/RaumSchiffeVersenken/AppTest.java
+++ src/test/java/RaumSchiffeVersenken/AppTest.java
@@ -2,14 +2,47 @@ package RaumSchiffeVersenken;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
+
     /**
      * Dummy test method
      */
+    @Test
+    public void testTreffer() {
+
+    }
+
+    @Test
+    public void testZielen() {
+
+    }
+
+    @Test
+    public void testSetzen() {
+
+    }
+
+    @Test
+    public void testMapErstellen() {
+
+    }
+
+    /** TODO
+     * testSpielerWechsel ( Mit abdecken und aufdecken der Karten beim Wechsel )
+     * testGewinnBedingung ( Alle Trefferpunkte des Gegners getroffen )
+     * testRevancheButton ( Spiel nach ordentlichen Durchgang neustarten)
+     * testSpielAbbruch ( Programm außerordentlich beenden )
+     * testen der Exceptions
+     */
+
+
+
+
     @Test
     public void testApp() {
         Assert.assertTrue(true);
