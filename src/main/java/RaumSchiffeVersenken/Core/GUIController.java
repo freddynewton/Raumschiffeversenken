8ec78412cb0d427254251303cf56ac602b639ed0
Georg Habermann <gh014@hdm-stuttgart.de>
2018-05-06 10:34:01 +0200

    FXML GridPanes as playing fields added

--- src/main/java/RaumSchiffeVersenken/Core/GUIController.java
+++ src/main/java/RaumSchiffeVersenken/Core/GUIController.java
@@ -1,4 +1,17 @@
 package RaumSchiffeVersenken.Core;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
 public class GUIController {
+
+    @FXML
+    private Pane spielfeld1;
+
+    @FXML
+    private Pane spielfeld2;
+
+
+
 }
