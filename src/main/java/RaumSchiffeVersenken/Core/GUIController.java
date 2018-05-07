c8b0624ea7a9b35605277d2235cf6ee3bd2fef5c
Georg Habermann <gh014@hdm-stuttgart.de>
2018-05-07 15:22:51 +0200

    added graphics in GridPane

--- src/main/java/RaumSchiffeVersenken/Core/GUIController.java
+++ src/main/java/RaumSchiffeVersenken/Core/GUIController.java
@@ -1,17 +1,54 @@
 package RaumSchiffeVersenken.Core;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.Button;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.function.DoubleToLongFunction;
 
-public class GUIController {
+public class GUIController implements Initializable {
 
     @FXML
-    private Pane spielfeld1;
+    private GridPane spielfeld1;
 
     @FXML
-    private Pane spielfeld2;
+    private GridPane spielfeld2;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        int feldSpalte = 10;
+        int feldReihe = 10;
 
+        //for-Schleife befüllt Spielfeld mit Buttons
+        for (int y = 0; y < feldSpalte; y++) {
+            for (int x = 0; x < feldReihe; x++) {
+                Image feldGrafik = new Image("File:images/weltraum.png");
+                ImageView grafik = new ImageView();
+                grafik.setFitWidth(24);
+                grafik.setFitHeight(24);
+                grafik.setImage(feldGrafik);
+                spielfeld1.add(grafik,x,y);
+            }
+        }
 
+        for (int y = 0; y < feldSpalte; y++) {
+            for (int x = 0; x < feldReihe; x++) {
+                Image feldGrafik = new Image("File:images/weltraum.png");
+                ImageView grafik = new ImageView();
+                grafik.setFitWidth(24);
+                grafik.setFitHeight(24);
+                grafik.setImage(feldGrafik);
+                spielfeld2.add(grafik,x,y);
+            }
+        }
+
+        //Methoden aus anderen Klassen implementieren
+        //Spielablauf spielablauf = new Spielablauf();
+        //spielablauf.SchiffeSetzen();
+    }
 }
--- src/main/java/RaumSchiffeVersenken/Core/SpielablaufFX.java
+++ src/main/java/RaumSchiffeVersenken/Core/SpielablaufFX.java
@@ -1,9 +1,11 @@
 package RaumSchiffeVersenken.Core;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -11,13 +13,15 @@ public class SpielablaufFX extends Application{
 
     @Override
     public void start(Stage spielefenster) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("gui.fxml"));
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getClassLoader().getResource("gui.fxml"));
+        Parent root = loader.load();
         Scene szene1 = new Scene(root);
         szene1.getStylesheets().add("gestaltung.css");
 
         spielefenster.setScene(szene1);
         spielefenster.show();
-    }
+     }
 
     public static void main(String[] args) {
         launch(args);
