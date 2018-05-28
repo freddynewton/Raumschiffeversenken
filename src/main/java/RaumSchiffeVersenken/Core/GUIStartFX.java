d7084072fba672e49e17b64e821cde608ba40da0
Georg Habermann <gh014@hdm-stuttgart.de>
2018-05-28 21:53:27 +0200

    -added new game window -added basis of scene handling

---
+++ src/main/java/RaumSchiffeVersenken/Core/GUIStartFX.java
@@ -0,0 +1,16 @@
+package RaumSchiffeVersenken.Core;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class GUIStartFX {
+
+    @FXML
+    private ImageView titelanimation;
+
+    private void animationStart(ActionEvent event) {
+
+    }
+
+}
