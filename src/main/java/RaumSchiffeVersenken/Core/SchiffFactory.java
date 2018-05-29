7e794a762a31cb0f3d286c9b03cfc8b2f13f126a
Jerg Jolanda <jj033@hdm-stuttgart.de>
2018-05-29 15:25:36 +0200

    Logger hinzugefügt in der Feld- und SchiffFactory-Klasse

--- src/main/java/RaumSchiffeVersenken/Core/SchiffFactory.java
+++ src/main/java/RaumSchiffeVersenken/Core/SchiffFactory.java
@@ -5,8 +5,22 @@ import RaumSchiffeVersenken.Core.SchiffsArten.Jaeger;
 import RaumSchiffeVersenken.Core.SchiffsArten.Zerstoerer;
 import RaumSchiffeVersenken.Core.SchiffsArten.Fregatte;
 import RaumSchiffeVersenken.Interface.RaumSchiff;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class SchiffFactory {
+
+
+    Lock lock = new ReentrantLock();
+
+    /**
+     *Logger erstellung.
+     */
+    private static final Logger log = LogManager.getLogger(Feld.class);
+
 
     /**
      * @param kriterium
@@ -15,19 +29,23 @@ public class SchiffFactory {
     public static RaumSchiff getRaumschiff(String kriterium) {
 
 
+
         if (kriterium.equals("1")) {
-            return new Jaeger() {
-            };
+            log.info("Jaeger wurde erstellt in der Factory");
+            return new Jaeger();
 
         } else if (kriterium.equals("2")) {
+            log.info("Bomber wurde erstellt in der Factory");
             return new Bomber() {
             };
 
         } else if (kriterium.equals("4")) {
+            log.info("Fregatte wurde erstellt in der Factory");
             return new Fregatte() {
             };
 
         } else if (kriterium.equals("5")) {
+            log.info("Zerstörer wurde erstellt in der Factory");
             return new Zerstoerer() {
             };
         }
