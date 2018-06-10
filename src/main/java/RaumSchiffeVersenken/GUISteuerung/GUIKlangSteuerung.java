e273d98b1eb54f57dd380f18e94892c6cb61e5a0
Georg Habermann <gh014@hdm-stuttgart.de>
2018-06-10 22:30:17 +0200

    -reformatted umlaute

--- src/main/java/RaumSchiffeVersenken/GUISteuerung/GUIKlangSteuerung.java
+++ src/main/java/RaumSchiffeVersenken/GUISteuerung/GUIKlangSteuerung.java
@@ -9,28 +9,28 @@ import java.io.File;
 public class GUIKlangSteuerung {
 
     public void knopfZielen() {
-        String musikDatei = "src/main/resources/geräusche/knopfZielen.wav";
+        String musikDatei = "src/main/resources/ton/knopfZielen.wav";
         Media knopfZielen = new Media(new File(musikDatei).toURI().toString());
         AudioClip musikSpieler = new AudioClip(knopfZielen.getSource());
         musikSpieler.play();
     }
 
     public void knopfDruecken() {
-        String musikDatei = "src/main/resources/geräusche/knopfDruecken.wav";
+        String musikDatei = "src/main/resources/ton/knopfDruecken.wav";
         Media knopfZielen = new Media(new File(musikDatei).toURI().toString());
         AudioClip musikSpieler = new AudioClip(knopfZielen.getSource());
         musikSpieler.play();
     }
 
     public void blaster() {
-        String musikDatei = "src/main/resources/geräusche/blaster.wav";
+        String musikDatei = "src/main/resources/ton/blaster.wav";
         Media knopfZielen = new Media(new File(musikDatei).toURI().toString());
         AudioClip musikSpieler = new AudioClip(knopfZielen.getSource());
         musikSpieler.play();
     }
 
     public void explosion() {
-        String musikDatei = "src/main/resources/geräusche/explosion.wav";
+        String musikDatei = "src/main/resources/ton/explosion.wav";
         Media knopfZielen = new Media(new File(musikDatei).toURI().toString());
         AudioClip musikSpieler = new AudioClip(knopfZielen.getSource());
         musikSpieler.play();
