7bc93fdb834a2171a3d3a1f3380481566b80a187
Georg Habermann <gh014@hdm-stuttgart.de>
2018-06-17 16:07:55 +0200

    -fixed warnings in SchiffsArten directory

--- src/main/java/RaumSchiffeVersenken/Interface_Factory/SchiffFactory.java
+++ src/main/java/RaumSchiffeVersenken/Interface_Factory/SchiffFactory.java
@@ -8,46 +8,36 @@ import RaumSchiffeVersenken.SchiffsArten.Fregatte;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 
 public class SchiffFactory {
-
-
-    Lock lock = new ReentrantLock();
 
     /**
      * Logger erstellung.
      */
     private static final Logger log = LogManager.getLogger(FeldFX.class);
 
-
     /**
-     * @param kriterium
+     * @param kriterium String
      * @return je nach Kriterium ein Schiff
      */
     public static RaumSchiff getRaumschiff(String kriterium) {
 
-
-        if (kriterium.equals("1")) {
-            log.info("Jaeger wurde erstellt in der Factory");
-            return new Jaeger();
-
-        } else if (kriterium.equals("2")) {
-            log.info("Bomber wurde erstellt in der Factory");
-            return new Bomber() {
-            };
-
-        } else if (kriterium.equals("4")) {
-            log.info("Fregatte wurde erstellt in der Factory");
-            return new Fregatte() {
-            };
-
-        } else if (kriterium.equals("5")) {
-            log.info("Zerstörer wurde erstellt in der Factory");
-            return new Zerstoerer() {
-            };
+        switch (kriterium) {
+            case "1":
+                log.info("Jaeger wurde erstellt in der Factory");
+                return new Jaeger();
+            case "2":
+                log.info("Bomber wurde erstellt in der Factory");
+                return new Bomber() {
+                };
+            case "4":
+                log.info("Fregatte wurde erstellt in der Factory");
+                return new Fregatte() {
+                };
+            case "5":
+                log.info("Zerstörer wurde erstellt in der Factory");
+                return new Zerstoerer() {
+                };
         }
 
         return null;
